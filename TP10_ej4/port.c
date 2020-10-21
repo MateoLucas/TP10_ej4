ad7ea3e098b5714e8946563fa61738c7bb1e6207
jandro1111 <jandro1111@jandro1111-VirtualBox>
2020-10-21 14:21:05 -0300

    creo q esta listo

--- TP10_ej4/port.c
+++ TP10_ej4/port.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #define INVALID(n,port) ((port=='a'&&n>7)||(port=='A'&&n>7)||(port=='b'&&n>7)||(port=='B'&&n>7)||(port=='d'&&n>15)||(port=='D'&&n>15)||(n<0))
-
+#define MAX1BYTE 255
 typedef union
 {
     struct portAB
@@ -205,4 +205,94 @@ int bitToggle(int n,char port)
         }
     }
     return errors;
+}
+/*maskOn: Dado un puerto y una máscara, debe prender todos aquellos bits que estén  prendidos  en  la  máscara,  sin  cambiar  el  estado  de  los  restantes. */
+int maskon(uint_least16_t mask,char port){
+    int on=0;// 0 si no hay error, 1 si hay error de mask, 2 si hay error de puerto
+    switch(port){
+        case'a':
+        case'A':
+            if(mask<=MAX1BYTE){
+                ports.portAB.portA|=mask;//hago un or entre la mascara y el puerto a, y se lo asigno al puerto a
+            }else{
+                on=1;//mask no valida
+            }
+            break;
+        case'B':
+        case'b':
+            if(mask<=MAX1BYTE){
+                ports.portAB.portB|=mask;//hago un or entre la mascara y el puerto b, y se lo asigno al puerto b
+            }else{
+                on=1;//mask no valida
+            }
+            break;
+        case'D':
+        case'd':
+            ports.portD |= mask;//hago un or entre la mascara y el puerto d, y se lo asigno al puerto d
+            break;
+        default:
+            on=2;
+            break;
+    }
+    return on;
+}
+/*maskOff: Dado un puerto y una máscara, debe apagar todos aquellos bits que estén  prendidos  en  la  máscara,  sin  cambiar  el  estado  de  los  restantes.*/
+int maskoff(uint_least16_t mask,char port){
+    int off=0;// 0 si no hay error, 1 si hay error de mask, 2 si hay error de puerto
+    switch(port){
+        case'a':
+        case'A':
+            if(mask<=MAX1BYTE){
+                ports.portAB.portA&=(~mask);//hago un and entre la mascara negada y el puerto a, y se lo asigno al puerto a
+            }else{
+                off=1;//mask no valida
+            }
+            break;
+        case'B':
+        case'b':
+            if(mask<=MAX1BYTE){
+                ports.portAB.portB&=(~mask);//hago un and entre la mascara negada y el puerto b, y se lo asigno al puerto b
+            }else{
+                off=1;//mask no valida
+            }
+            break;
+        case'D':
+        case'd':
+            ports.portD &= (~mask);//hago un and entre la mascara negada y el puerto d, y se lo asigno al puerto d
+            break;
+        default:
+            off=2;
+            break;
+    }
+    return off;
+}
+/*maskToggle: Dado un puerto y una máscara, debe cambiar el estado de todos aquellos  bits  que  estén  prendidos  en  la  máscara  al  opuesto,  sin  cambiar  el estado de los restantes.*/
+int masktoggle(uint_least16_t mask,char port){
+    int toggle=0;// 0 si no hay error, 1 si hay error de mask, 2 si hay error de puerto
+    switch(port){
+        case'a':
+        case'A':
+            if(mask<=MAX1BYTE){
+                ports.portAB.portA^=mask;//hago un xor entre la mascara y el puerto a, y se lo asigno al puerto a
+            }else{
+                toggle=1;//mask no valida
+            }
+            break;
+        case'B':
+        case'b':
+            if(mask<=MAX1BYTE){
+                ports.portAB.portB^=mask;//hago un xor entre la mascara y el puerto b, y se lo asigno al puerto b
+            }else{
+                toggle=1;//mask no valida
+            }
+            break;
+        case'D':
+        case'd':
+            ports.portD ^= mask;//hago un xor entre la mascara y el puerto d, y se lo asigno al puerto d
+            break;
+        default:
+            toggle=2;
+            break;
+    }
+    return toggle;
 }
